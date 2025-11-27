263550c9ce0a2422be86d5a76fa2ba58e0e599c9
Crayzeecodes <zeeshanskhan822@gmail.com>
2025-11-27 15:22:44 +0530

    27/11/25

---
+++ Parkinsons/View/AddMedicationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddMedicationTableViewCell.swift
+//  Parkinsons
+//
+//  Created by SDC-USER on 27/11/25.
+//
+
+import UIKit
+
+class AddMedicationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
