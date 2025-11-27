c85614bf1cd09927b8c419c6e9f889fb68b9f518
Nevin Abraham <nevinabr025@gmail.com>
2025-11-27 17:14:35 +0530

    27/11/25

---
+++ Parkinsons/Controller/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  Parkinsons
+//
+//  Created by SDC-USER on 27/11/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var calenderArea: CalenderCollectionViewCell!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//extension HomeViewController: UICollectionViewDataSource {
+//    
+//}
---
+++ Parkinsons/View/CalenderCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CalenderCollectionViewCell.swift
+//  Parkinsons
+//
+//  Created by SDC-USER on 27/11/25.
+//
+
+import UIKit
+
+class CalenderCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var calenderDate: UILabel!
+    @IBOutlet weak var calenderBackground: UIView!
+    @IBOutlet weak var calenderDay: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
