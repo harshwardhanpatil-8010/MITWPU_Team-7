94fa399d955850ba10115e8e12752a9e378f8705
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-12-04 23:47:09 -0800

    5/12/25

--- Parkinsons/Controller/10minworkoutLandingPageViewController.swift
+++ Parkinsons/Controller/10minworkoutLandingPageViewController.swift
@@ -45,8 +45,6 @@ class _0minworkoutLandingPageViewController: UIViewController, UITableViewDataSo
         // Pass the selected object to the new view controller.
     }
     */
-    @objc func infoButtonTapped() {
-        
-    }
+
 
 }
