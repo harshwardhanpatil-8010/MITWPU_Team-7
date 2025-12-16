aa6d3f67c4312d662143c08cafefc9c7ecda2267
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2025-12-16 01:47:55 -0800

    16/12/25

--- Parkinsons/Controller/Rhythmic Walking/SessionRunningViewController.swift
+++ Parkinsons/Controller/Rhythmic Walking/SessionRunningViewController.swift
@@ -56,6 +56,7 @@ class SessionRunningViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Rhythmic Walking"
         setupProgressView()
         progressView.progressColor = UIColor(hex: "90AF81")
         progressView.trackColor = .systemGray5
