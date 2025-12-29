468b7a12d653e00897a571e9c26f9632827885e7
nevin <nevin.abraham@mitwpu.edu.in>
2025-12-29 11:35:13 +0530

    Feat: Collection view update and files created

---
+++ Parkinsons/View/tremorCard.swift
@@ -0,0 +1,19 @@
+//
+//  tremorCard.swift
+//  Parkinsons
+//
+//  Created by Noya Abraham on 27/12/25.
+//
+
+import UIKit
+
+class tremorCard: UICollectionViewCell {
+
+    @IBOutlet weak var avgLabel: UILabel!
+    @IBOutlet weak var cardBackground: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
