5cefb89a2b8b25688136961e9cf517bc4e415350
Crayzeecodes <zeeshanskhan822@gmail.com>
2026-01-07 11:50:16 +0530

    7/1/26

---
+++ Parkinsons/View/Symptom Recording/tremorCard.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class tremorCard: UICollectionViewCell {
+
+    @IBOutlet weak var avgLabel: UILabel!
+    @IBOutlet weak var cardBackground: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Match MedicationCard: Disable clipping to allow shadows to show
+        self.clipsToBounds = false
+        self.contentView.clipsToBounds = false
+        
+        setupCardStyle()
+    }
+    
+    func setupCardStyle() {
+        // Exact same parameters from MedicationCardCollectionViewCell
+        let cornerRadius: CGFloat = 16
+        let shadowColor: UIColor = .black
+        let shadowOpacity: Float = 0.15
+        let shadowRadius: CGFloat = 3
+        let shadowOffset: CGSize = .init(width: 0, height: 1)
+
+        cardBackground.layer.cornerRadius = cornerRadius
+        cardBackground.layer.masksToBounds = false // Important for shadow visibility
+
+        cardBackground.layer.shadowColor = shadowColor.cgColor
+        cardBackground.layer.shadowOpacity = shadowOpacity
+        cardBackground.layer.shadowRadius = shadowRadius
+        cardBackground.layer.shadowOffset = shadowOffset
+    }
+    
+    // Optional: Add a configure method like your other cells
+    func configure(average: String) {
+        avgLabel.text = average
+    }
+}
