2bd99098a0bf89ce4bf83a130bb0e2023ac54e2f
harshwardhanpatil-8010 <harshwardhan.patil2005@gmail.com>
2026-02-26 10:35:45 +0530

    Feat: symptom DB

---
+++ CDSymptomLog+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  CDSymptomLog+CoreDataClass.swift
+//  Parkinsons
+//
+//  Created by SDC-USER on 26/02/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias CDSymptomLogCoreDataClassSet = NSSet
+
+@objc(CDSymptomLog)
+public class CDSymptomLog: NSManagedObject {
+
+}
---
+++ CDSymptomLog+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  CDSymptomLog+CoreDataProperties.swift
+//  Parkinsons
+//
+//  Created by SDC-USER on 26/02/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+
+public typealias CDSymptomLogCoreDataPropertiesSet = NSSet
+
+extension CDSymptomLog {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDSymptomLog> {
+        return NSFetchRequest<CDSymptomLog>(entityName: "CDSymptomLog")
+    }
+
+    @NSManaged public var date: Date?
+    @NSManaged public var id: UUID?
+    @NSManaged public var notes: String?
+    @NSManaged public var severity: Int16
+    @NSManaged public var symptom: Int16
+
+}
+
+extension CDSymptomLog : Identifiable {
+
+}
